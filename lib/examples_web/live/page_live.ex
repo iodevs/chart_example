7417f781fff0af69607393dbc701aa81d50343ce
Ondrej Tucek <ondrej.tucek@gmail.com>
2020-07-19 22:34:42 +0200

    Add gauge skeleton

---
+++ lib/examples_web/live/page_live.ex
@@ -0,0 +1,38 @@
+defmodule ExamplesWeb.PageLive do
+  use ExamplesWeb, :live_view
+  alias Examples.Gauge
+
+  @impl true
+  def mount(_params, _session, socket) do
+    generate_value()
+
+    {:ok, assign(socket, gauge: Gauge.setup([]))}
+  end
+
+  # @impl true
+  # def handle_event("suggest", %{"q" => query}, socket) do
+  #   {:noreply, assign(socket, results: search(query), query: query)}
+  # end
+
+  @impl true
+  def handle_info(:gen_val, socket) do
+    g = Gauge.put(socket.assigns.gauge, Enum.random(1..100))
+    render(socket.assigns)
+    generate_value()
+
+    {:noreply, assign(socket, gauge: g)}
+  end
+
+  @impl true
+  def render(assigns) do
+    assigns = [gauge_graph: Gauge.render(assigns.gauge)]
+
+    Phoenix.View.render(ExamplesWeb.PageView, "page_live.html", assigns)
+  end
+
+  # Private
+
+  defp generate_value() do
+    Process.send_after(self(), :gen_val, 2_000)
+  end
+end
