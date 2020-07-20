3b4c963353bf87edabac9b698e9534b6d71aa2da
ondrej-tucek <ondrej.tucek@gmail.com>
2020-07-20 10:58:19 +0200

    Add text value label into gauge

--- lib/examples/gauge/svg.ex
+++ lib/examples/gauge/svg.ex
@@ -13,16 +13,16 @@ defmodule Examples.Gauge.Svg do
       viewbox="0 0 <%= elem(@settings.viewbox, 0) %> <%= elem(@settings.viewbox, 1) %>" >
 
       <defs>
-      <line id="major-line-ticks"
-        x1="<%= @settings.major_ticks.length %>"
-        transform="<%= @settings.major_ticks.translate %>" />
-    </defs>
-    <g class="major-ticks">
-      <%= for angle <- @settings.major_ticks.positions do %>
-        <use xlink:href="#major-line-ticks"
-          transform="rotate(<%= angle %>, <%= elem(@settings.gauge_center, 0) %>, <%= elem(@settings.gauge_center, 1) %>)" />
-      <% end %>
-    </g>
+        <line id="major-line-ticks"
+          x1="<%= @settings.major_ticks.length %>"
+          transform="<%= @settings.major_ticks.translate %>" />
+      </defs>
+      <g class="major-ticks">
+        <%= for angle <- @settings.major_ticks.positions do %>
+          <use xlink:href="#major-line-ticks"
+            transform="rotate(<%= angle %>, <%= elem(@settings.gauge_center, 0) %>, <%= elem(@settings.gauge_center, 1) %>)" />
+        <% end %>
+      </g>
 
       <g class="gauge">
         <path id="gauge-bg-border"
@@ -35,6 +35,13 @@ defmodule Examples.Gauge.Svg do
           d="<%= @settings.d_value %>">
         </path>
       </g>
+
+      <text class="value-text"
+        x="<%= elem(@settings.text_value_position, 0) %>" y = "<%= elem(@settings.text_value_position, 1) %>"
+        text-anchor="middle" alignment-baseline="middle" dominant-baseline="central">
+          <%= @settings.text_value %>
+      </text>
+
     </svg>
     """
   end
