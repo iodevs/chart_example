659b4d0932339ff038ea71eb71bb2270bc074710
ondrej-tucek <ondrej.tucek@gmail.com>
2020-10-11 08:25:03 +0200

    Add heroku dependency files

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,11 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :examples, ExamplesWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [
+    scheme: "https",
+    host: "chart.herokuapp.com",
+    port: 443
+  ],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
