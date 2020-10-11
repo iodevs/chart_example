9773049cdec44cc3ef1fe2b11b3f30bedddf5fec
ondrej-tucek <ondrej.tucek@gmail.com>
2020-10-11 18:09:21 +0200

    Fix chart dependency

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule Examples.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      chart_lib(Mix.env(), "v0.1.0")
+      chart_lib(Mix.env(), "0.1.0")
     ]
   end
 
